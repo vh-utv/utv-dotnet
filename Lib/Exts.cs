b815fd87948bd44c606e83f11958f17a0d5cd255
Hoang Vu <contact@hoang.vu>
2021-02-22 09:41:11 +0100

    initial commit

---
+++ Lib/Exts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UtvDotNet.Lib
+{
+    public static class Exts
+    {
+        public static IEnumerable<TResult> Map<TSource, TResult>(this IEnumerable<TSource> input, Func<TSource, TResult> mapper)
+        {
+            throw new NotImplementedException("TODO: implement this");
+        }
+
+        public static IEnumerable<TResult> FlatMap<TSource, TResult>(this IEnumerable<TSource> input, Func<TSource, IEnumerable<TResult>> mapper)
+        {
+            throw new NotImplementedException("TODO: implement this");
+        }
+
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> input, Func<T, bool> filter)
+        {
+            throw new NotImplementedException("TODO: implement this");
+        }
+
+        public static TAccumulate Reduce<T, TAccumulate>(this IEnumerable<T> input, T seed, Func<TAccumulate, T, TAccumulate> func)
+        {
+            throw new NotImplementedException("TODO: implement this");
+        }
+    }
+}
